273963a71457e1e42f9a5a6e91ec7f2aaa1bdcd7
UmidKhodzhaev <umidik3@gmail.com>
2019-01-20 21:00:42 +0700

    Created project
    Created project

---
+++ Game/desktop/src/com/ukec/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ukec.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ukec.game.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
